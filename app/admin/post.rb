cfcc9896d9716f0aa9b9449cca6ddc19213e139d
jeffreybiles <bilesjeffrey@gmail.com>
2014-05-12 20:12:03 -0500

    publish_date can be saved and shows up on the index

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Post do
-  permit_params :title, :date_published, :video_url, :description, :transcript
+  permit_params :title, :publish_date, :video_url, :description, :transcript
   
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
