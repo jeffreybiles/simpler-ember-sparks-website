5bc5e8cadb791b73d33e0fb5cc66ce3a0153300c
jeffreybiles <bilesjeffrey@gmail.com>
2014-05-13 08:32:01 -0500

    Link Editing! Markdown for everyone!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :markdown_renderer
+
+  def markdown_renderer
+    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true)
+  end
 end
