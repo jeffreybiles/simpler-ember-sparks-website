7185e736b54e2c960f0a21ced4157de12768e2d3
jeffreybiles <bilesjeffrey@gmail.com>
2015-02-20 21:43:08 -0600

    create user and sign in when you create subscription

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,16 +1,24 @@
 class AccountsController < ApplicationController
+  include Devise::Controllers::SignInOut
+
   def show
 
   end
 
   def subscribe
+    unless current_user
+      user = User.create(email: params[:stripeEmail], password: 'temporaryPassword')
+      sign_in(user)
+      # send an email
+    end
     customer = Stripe::Customer.create(
       :card => params[:stripeToken],
       :plan => "pro",
       :email => current_user.email
     )
     current_user.update(stripe_customer_id: customer.id, subscribed: true)
-    redirect_to 'show'
+
+    redirect_to account_path
   end
 
   def unsubscribe
@@ -19,6 +27,6 @@ class AccountsController < ApplicationController
       customer.subscriptions.retrieve(subscription.id).delete
     end
     current_user.update(subscribed: false)
-    redirect_to 'show'
+    redirect_to account_path
   end
 end
