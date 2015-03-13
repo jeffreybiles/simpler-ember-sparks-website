88d28fbc933db47ec77e859f49dd49a542c29b15
jeffreybiles <bilesjeffrey@gmail.com>
2015-03-12 22:11:56 -0500

    now maybe send grid will actually send

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,12 +64,13 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'emberscreencasts.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net' }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
-  
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
