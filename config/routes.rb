19e23a88742b523cfebfa98ce42021e6940d32d8
jeffreybiles <bilesjeffrey@gmail.com>
2015-04-01 01:11:48 -0500

    Add series

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :posts, only: [:show, :index]
+  get 'series', to: 'pages#series'
   get 'tags/:tag', to: 'posts#index', as: :tag
   get 'about', to: 'pages#about'
   get 'mailing_list', to: 'pages#mailing_list'
