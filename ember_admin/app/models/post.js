390312524653a5fb09cb7c6d899523ad6aec8e33
jeffreybiles <bilesjeffrey@gmail.com>
2015-04-19 20:34:10 -0500

    View posts on Ember.  Next: View tags

---
+++ ember_admin/app/models/post.js
@@ -0,0 +1,14 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  title: DS.attr('string'),
+  video_url: DS.attr('string'),
+  description: DS.attr('string'),
+  transcript: DS.attr('string'),
+  publishDate: DS.attr('date'),
+  updatedAt: DS.attr('date'),
+  createdAt: DS.attr('date'),
+  thumbnailImage: DS.attr('string'),
+  permalink: DS.attr('string'),
+  free: DS.attr('boolean')
+});
--- ember_admin/app/router.js
+++ ember_admin/app/router.js
@@ -6,4 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 export default Router.map(function() {
+  this.resource('posts', function() {
+    this.resource('post', {path: ':post_id'});
+  });
 });
