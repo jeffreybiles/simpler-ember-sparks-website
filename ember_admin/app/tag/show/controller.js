ac2f2f436147b120f8b7f3a58e5df3fe36d2a619
jeffreybiles <bilesjeffrey@gmail.com>
2015-04-26 15:49:10 -0500

    deleted old unused methods

--- ember_admin/app/tag/show/controller.js
+++ ember_admin/app/tag/show/controller.js
@@ -2,14 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
-    edit: function(){
-      this.set('isEditing', true)
-    },
-    save: function(){
-      this.get("model").save().then( ()=>{
-        this.set('isEditing', false)
-      })
-    },
     delete: function(){
       this.get("model").destroyRecord().then( ()=>{
         this.transitionToRoute('tags')
