7e7c3c83899e8dde936a1643dbee0af56e57f44a
jeffreybiles <bilesjeffrey@gmail.com>
2015-04-30 22:01:56 -0500

    change tags and blueprint to not need route

--- ember_admin/app/tags/new/controller.js
+++ ember_admin/app/tags/new/controller.js
@@ -1,9 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  newTag: {
+    name: '',
+    description: '',
+    tagType: '',
+    priority: ''
+  },
+
   actions: {
     save: function(){
-      var tag = this.store.createRecord('tag', this.get('model'));
+      var tag = this.store.createRecord('tag', this.get('newTag'));
       tag.save().then((response)=>{
         this.transitionTo('tag.show', response)
       })
--- ember_admin/blueprints/crud/files/__root__/__name__s/new/controller.js
+++ ember_admin/blueprints/crud/files/__root__/__name__s/new/controller.js
@@ -1,9 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  new<%= classifiedModuleName %>: {
+    // properties here
+  },
   actions: {
     save: function(){
-      var <%= classifiedModuleName %> = this.store.createRecord('<%= dasherizedModuleName %>', this.get('model'));
+      var <%= classifiedModuleName %> = this.store.createRecord('<%= dasherizedModuleName %>', this.get('new<%= classifiedModuleName %>'));
       <%= classifiedModuleName %>.save().then((response)=>{
         this.transitionTo('<%= dasherizedModuleName %>.show', response)
       })
