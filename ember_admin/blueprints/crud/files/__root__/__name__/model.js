c569e65560048777fad15edd19deeed32ff78216
jeffreybiles <bilesjeffrey@gmail.com>
2015-05-03 18:17:22 -0600

    takes the attributes, adds them to model, show, and edit-fields
    next: add stuff to router

--- ember_admin/blueprints/crud/files/__root__/__name__/model.js
+++ ember_admin/blueprints/crud/files/__root__/__name__/model.js
@@ -1,4 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  <%= modelAttrs %>,
+
+  //this is what's used when listing records
+  toS: Ember.computed.alias('name') 
+  }
 })
--- ember_admin/blueprints/crud/index.js
+++ ember_admin/blueprints/crud/index.js
@@ -1,14 +1,71 @@
+var inflection  = require('inflection');
+var stringUtils = require('../../lib/utilities/string');
+var EOL         = require('os').EOL;
+
 module.exports = {
-  description: 'generates a CRUD setup'
+  description: 'generates a CRUD setup',
 
-  // locals: function(options) {
-  //   // Return custom template variables here.
-  //   return {
-  //     foo: options.entity.options.foo
-  //   };
-  // }
+  anonymousOptions: [
+    'name',
+    'attr:type'
+  ],
 
-  // afterInstall: function(options) {
-  //   // Perform extra work here.
-  // }
-};
+  locals: function(options) {
+    var modelAttrs = [];
+    var inputAttrs = [];
+    var displayAttrs = [];
+
+    var entityOptions = options.entity.options;
+
+    for (var name in entityOptions) {
+      var type = entityOptions[name] || '';
+      var dasherizedName = stringUtils.dasherize(name);
+      var camelizedName = stringUtils.camelize(name);
+      var dasherizedType = stringUtils.dasherize(type);
+
+      modelAttrs.push(camelizedName + ': ' + dsAttr(dasherizedName, dasherizedType));
+      inputAttrs.push("  <div class='crud-input'>" + inputField(dasherizedName, dasherizedType) + "</div>")
+      displayAttrs.push("  <div class='crud-attr'>" + display(dasherizedName, dasherizedType) + "</div>")
+
+    }
+
+    modelAttrs = modelAttrs.join(',' + EOL + '  ');
+    inputAttrs = inputAttrs.join(EOL);
+    displayAttrs = displayAttrs.join(EOL);
+
+    return {
+      modelAttrs: modelAttrs,
+      inputAttrs: inputAttrs,
+      displayAttrs: displayAttrs
+    };
+  }
+}
+
+function dsAttr(name, type) {
+  switch (type) {
+  case 'boolean':
+  case 'date':
+  case 'number':
+  case 'string':
+    return 'DS.attr(\'' + type + '\')';
+  default:
+    //"If you don't specify the type of the attribute, it will be whatever was provided by the server"
+    //http://emberjs.com/guides/models/defining-models/
+    return 'DS.attr()';
+  }
+}
+
+function inputField(name, type) {
+  switch (type) {
+    case 'boolean':
+      return "<input type='checkbox' checked=" + name + ">"
+    case 'date': 
+      return "{{--! Put your date code here --}}"
+    default:
+      return "<input type='text' value=" + name + ">"
+  }
+}
+
+function display(name, type) {
+  return name + ": {{" + name + "}}"
+}
