3d77cde8c0cb32bd508bef40a0615f8dfe7419c1
jeffreybiles <bilesjeffrey@gmail.com>
2015-05-28 03:25:40 -0500

    Well.  That was easy.

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'posts#index'
+  root 'pages#home'
 
   namespace :api do
     resources :posts
