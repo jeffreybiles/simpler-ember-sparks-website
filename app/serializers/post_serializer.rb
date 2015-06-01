24febd0e6350bbeb3ba17f6e26bd0ca8d051da3a
jeffreybiles <bilesjeffrey@gmail.com>
2015-05-31 21:17:16 -0500

    solved the 'disappearing seconds' problem

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,7 +1,7 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :title, :video_url, :description, :transcript, 
               :publish_date, :created_at, :updated_at,
-              :thumbnail_image, :permalink, :free, :difficulty
+              :thumbnail_image, :permalink, :free, :difficulty, :seconds
 
   has_many :taggings, embed: :ids, include: true
 end
