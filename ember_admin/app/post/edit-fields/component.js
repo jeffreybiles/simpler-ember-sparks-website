1b965050c6448ead7d341d5f5f4b9f7c60444406
jeffreybiles <bilesjeffrey@gmail.com>
2015-06-24 21:54:37 -0500

    get rid of the observer!  Woo!

--- ember_admin/app/post/edit-fields/component.js
+++ ember_admin/app/post/edit-fields/component.js
@@ -1,22 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  previewImage: Ember.observer('model.temporaryThumbnailImage', function(){
-    var file = this.get('model.temporaryThumbnailImage')
-    if (file) {
-      var reader = new FileReader();
+  previewImage(file) {
+    var reader = new FileReader();
 
-      reader.onload = function (e) {
-        $('#preview-image').attr('src', e.target.result);
-      }
-
-      reader.readAsDataURL(file);
+    reader.onload = function (e) {
+      $('#preview-image').attr('src', e.target.result);
     }
-  }),
+
+    reader.readAsDataURL(file);
+  },
 
   actions: {
-    receiveFile: function(file){
+    receiveFile(file) {
       this.set('model.temporaryThumbnailImage', file);
+      this.previewImage(file);
     }
   }
 })
