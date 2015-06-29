72ffb892c45b9b040166ee33842301a8de4bc04f
jeffreybiles <bilesjeffrey@gmail.com>
2015-06-29 11:39:10 -0500

    fingerprint prepend

--- ember_admin/Brocfile.js
+++ ember_admin/Brocfile.js
@@ -2,7 +2,11 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
+var app = new EmberApp({
+  fingerprint: {
+    prepend: 'https://s3.amazonaws.com/spark-casts/'
+  }
+});
 
 
 app.import('bower_components/underscore/underscore-min.js');
