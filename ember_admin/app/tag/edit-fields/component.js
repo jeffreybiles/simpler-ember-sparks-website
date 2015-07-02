ec4b93df6f1483d7a2e75556fab5cdb8cbf658f1
jeffreybiles <bilesjeffrey@gmail.com>
2015-07-02 18:43:46 -0500

    Select in vanilla Ember

--- ember_admin/app/tag/edit-fields/component.js
+++ ember_admin/app/tag/edit-fields/component.js
@@ -1,5 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagTypes: ['series', 'subject']
+  tagTypes: ['series', 'subject'],
+
+  actions: {
+    selectTagType() {
+      const selectedEl = this.$('#tag-type-select')[0];
+      const selectedIndex = selectedEl.selectedIndex;
+      const options = $('#tag-type-select option');
+      const selectedValue = options[selectedIndex].value;
+
+      this.set('model.tagType', selectedValue);
+    }
+  }
 });
