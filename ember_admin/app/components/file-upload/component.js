733350a95d73db915509bed85cf620cecca672fb
jeffreybiles <bilesjeffrey@gmail.com>
2015-07-02 11:07:45 -0500

    dragEnter and dragLeave

--- ember_admin/app/components/file-upload/component.js
+++ ember_admin/app/components/file-upload/component.js
@@ -3,6 +3,16 @@ import Ember from 'ember';
 export default Ember.Component.extend({  
   tagName: 'div',
   classNames: ['uploader', 'dropzone'],
+  isDragging: Ember.computed.gt('dragCounter', 0),
+  classNameBindings: ['isDragging'],
+  dragCounter: 0,
+
+  dragEnter() {
+    this.incrementProperty('dragCounter')
+  },
+  dragLeave(){
+    this.decrementProperty('dragCounter')
+  },
 
   dragOver(event) {
     event.preventDefault()
@@ -10,6 +20,8 @@ export default Ember.Component.extend({
 
   drop(event) {
     event.preventDefault()
+
+    this.set('dragCounter', 0)
 
     var file = event.dataTransfer.files[0]
     this.sendAction('fileInputChanged', file)
