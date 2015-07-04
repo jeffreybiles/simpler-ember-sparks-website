fbbddcf4453319bed47ef705dace788f66fb7df5
jeffreybiles <bilesjeffrey@gmail.com>
2015-07-04 17:14:16 -0500

    x-select

--- ember_admin/app/tag/edit-fields/component.js
+++ ember_admin/app/tag/edit-fields/component.js
@@ -4,13 +4,8 @@ export default Ember.Component.extend({
   tagTypes: ['series', 'subject'],
 
   actions: {
-    selectTagType() {
-      const selectedEl = this.$('#tag-type-select')[0]
-      const selectedIndex = selectedEl.selectedIndex;
-      const options = $('#tag-type-select option');
-      const selectedValue = options[selectedIndex].value;
-
-      this.set('model.tagType', selectedValue);
+    selectTagType(value, component) {
+      this.set('model.tagType', value);
     }
   }
 });
