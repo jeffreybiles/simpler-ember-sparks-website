0ee29be9878f9fff9f48f3a1fc7cf3796977cd9a
jeffreybiles <bilesjeffrey@gmail.com>
2015-07-21 11:22:13 -0500

    Using computed property as a property

--- ember_admin/app/demonstrations/computed-getter/controller.js
+++ ember_admin/app/demonstrations/computed-getter/controller.js
@@ -1,9 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  firstName: '',
-  lastName: '',
+  firstName: 'Tony',
+  lastName: 'Stark',
   fullName: Ember.computed('firstName', 'lastName', function(){
     return `${this.get('firstName')} ${this.get('lastName')}`;
+  }),
+  companyName: 'Stark Industries',
+  businessCard: Ember.computed('fullName', 'companyName', function(){
+    return `${this.get('fullName')} of ${this.get('companyName')}`
   })
 });
