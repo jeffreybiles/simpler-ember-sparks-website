44a6142311cb8508367ac3d22ad27e9b4755a534
jeffreybiles <bilesjeffrey@gmail.com>
2015-07-30 23:22:33 -0500

    content security policy

--- ember_admin/config/environment.js
+++ ember_admin/config/environment.js
@@ -16,6 +16,16 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    contentSecurityPolicy: {
+      'default-src': "'self' http://platform.twitter.com",
+      'script-src': "'self' http://www.google-analytics.com/analytics.js http://platform.twitter.com/widgets.js http://i.kissmetrics.com/i.js http://doug1izaerwt3.cloudfront.net",
+      'font-src': "'self' https://fonts.gstatic.com",
+      'connect-src': "'self'",
+      'img-src': "'self' http://www.google-analytics.com http://trk.kissmetrics.com https://spark-casts.s3.amazonaws.com",
+      'style-src': "'self' https://fonts.googleapis.com",
+      'media-src': "'self'"
     }
   };
 
