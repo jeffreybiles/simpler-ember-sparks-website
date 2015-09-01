df92ec3a35ba97bfd458132dbf5666c69fde5908
jeffreybiles <bilesjeffrey@gmail.com>
2015-09-01 12:11:55 -0500

    action parameters

--- ember_admin/app/demonstrations/actions/controller.js
+++ ember_admin/app/demonstrations/actions/controller.js
@@ -2,9 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   wearingAHat: true,
+  hatColor: "blue",
+  hatColors: ["blue", "green", "red", "purple", "orange"],
   actions: {
-    putOnHat(){
+    putOnHat(color){
       this.set('wearingAHat', true)
+      this.set('hatColor', color)
     },
     takeOffHat(){
       this.set('wearingAHat', false)
