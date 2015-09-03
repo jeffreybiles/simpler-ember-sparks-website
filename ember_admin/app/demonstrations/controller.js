414567d63f663f1d19ee0c77a6e54e8588fd6960
jeffreybiles <bilesjeffrey@gmail.com>
2015-09-03 02:08:44 -0500

    bubbling chain

---
+++ ember_admin/app/demonstrations/controller.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+    receiveAdvice(){
+      alert('Make sure your customers are happy and your business model is sound.')
+    }
+  }
+})
---
+++ ember_admin/app/demonstrations/route.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  actions: {
+    receiveAdvice(){
+      alert('Scale! Eyeballs! Unicorns!... Can you make hats for unicorns?  Synergy.');
+    },
+  }
+})
