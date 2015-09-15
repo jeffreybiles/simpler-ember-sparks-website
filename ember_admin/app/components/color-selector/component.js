da8a5ab4dcf729b81ab5db3a21e0b7350fbb82f0
jeffreybiles <bilesjeffrey@gmail.com>
2015-09-15 03:31:56 -0500

    closure actions

--- ember_admin/app/components/color-selector/component.js
+++ ember_admin/app/components/color-selector/component.js
@@ -4,7 +4,7 @@ export default Ember.Component.extend({
   colors: ["blue", "red", "green", "purple", "orange", "none"],
   actions: {
     changeItem(color){
-      this.sendAction('changeItem', color)
+      this.attrs.changeItem(color)
     }
   }
 });
--- ember_admin/app/components/control-accessory/component.js
+++ ember_admin/app/components/control-accessory/component.js
@@ -2,10 +2,4 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   noItem: Ember.computed.equal('itemColor', 'none'),
-
-  actions: {
-    changeItem(color){
-      this.sendAction('changeItem', color)
-    }
-  }
 })
