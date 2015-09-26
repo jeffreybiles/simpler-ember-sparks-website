b98e87d89f0e9f679543c5924a5b2f74d15a5b7c
jeffreybiles <bilesjeffrey@gmail.com>
2015-09-26 15:21:59 -0500

    data from the rails controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def about
 
   end
+
+  def ember_admin
+    @accessories = ['hats', 'boots', 'scarves'] #this could easily be from the database
+  end
 end
