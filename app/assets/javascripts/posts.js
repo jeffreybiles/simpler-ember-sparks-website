906fce4af42f5db5d0e0274a2c4d78d2031addcf
jeffreybiles <bilesjeffrey@gmail.com>
2015-09-29 17:55:42 -0500

    side-by-side video and info; no more hanging banner; changed it so we don't stomp all over the poor innocent h1s

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -2,10 +2,8 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
-console.log('is this thing on?')
 $(document).on('click', '.clickable-media-link', function(event){
   console.log("it got a click")
   path = $(this).data('url')
-  console.log(path)
   window.location.href = path
 })
