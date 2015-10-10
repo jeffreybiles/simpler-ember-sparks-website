57910a4efacad6015e8d4cabe405e6bfe4d00267
jeffreybiles <bilesjeffrey@gmail.com>
2015-10-09 21:42:47 -0500

    temporarily render without images, since I can't figure out a good way to do it yet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,15 @@ class ApplicationController < ActionController::Base
   before_action :markdown_renderer
 
   def markdown_renderer
-    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true)
+    @markdown ||= Redcarpet::Markdown.new(RenderWithNewPhotos, autolink: true)
+  end
+end
+
+class RenderWithNewPhotos < Redcarpet::Render::HTML
+  def image(link, title, alt_text)
+    return ''
+    # We'll have to figure out a way to get the images out of ghost
+    # For now, we'll just remove them
+    # return "<img src='http://emberscreencast-markdown-editing-dont-publish.ghost.io#{link}' title='#{title}' alt='#{alt_text}'"
   end
 end
