a149cd5281dd16e11aa547c3600a1a4d25f2a697
jeffreybiles <bilesjeffrey@gmail.com>
2015-10-17 20:49:19 -0500

    no more infinite loops

--- app/serializers/organization_serializer.rb
+++ app/serializers/organization_serializer.rb
@@ -1,5 +1,5 @@
 class OrganizationSerializer < ActiveModel::Serializer
   attributes :id, :name, :subscribed
 
-  has_many :users
+  has_many :users, embed: :ids, include: true
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :organization_permission_level
-
-  has_one :organization
+  attributes :id, :email, :organization_permission_level, :organization_id
 end
