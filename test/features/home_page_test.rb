f656c87ecb6503c64eabd50d0fa282d95860a0a2
jeffreybiles <bilesjeffrey@gmail.com>
2016-01-31 22:40:33 -0600

    assert that two newest videos showing up

--- test/features/home_page_test.rb
+++ test/features/home_page_test.rb
@@ -3,7 +3,9 @@ require "test_helper"
 class HomePageTest < Capybara::Rails::TestCase
   test "sanity" do
     visit root_path
-    assert_content page, "Twice-weekly screencasts"
+    assert_content page.find_all('h1').first, "Twice-weekly screencasts"
     refute_content page, "Hello World!"
+    assert_content page.find_by_id('newest_free_video'), "10: My last free video"
+    assert_content page.find_by_id('newest_pro_video'), "11: My last pro video"
   end
 end
