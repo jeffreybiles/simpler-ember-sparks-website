ceaf2b733421080645ce7a736c94cd5d404d401b
jeffreybiles <bilesjeffrey@gmail.com>
2016-02-15 23:49:01 -0600

    No images 4 u, db seeders

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -19,8 +19,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
      # For Rails 3.1+ asset pipeline compatibility:
-    ActionController::Base.helpers.asset_path("fallback_thumbnail.jpg")
-  
+    # ActionController::Base.helpers.asset_path("fallback_thumbnail.jpg")
+    "fallback_thumbnail.jpg"
     # "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   end
 
