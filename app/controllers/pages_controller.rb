e400a70d79f4ada067c42253e8f45f832b223459
Michael Lobel <mglobel@gmail.com>
2016-03-12 16:01:07 -0600

    dedicated archive page
    @jeffreybiles we were totes on the right path, just needed to clear the
    cache to see the changes (or open in incognito). a little spacing and
    vertical align though and i think looks good:
    https://www.dropbox.com/s/swg2z74n0xaat8u/Screenshot%202016-03-12%2016.0
    0.25.png?dl=0

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,10 @@ class PagesController < ApplicationController
 
   end
 
+  def archive
+    @posts = Post.all.published.recent_first
+  end
+
   def meta_guide_to_upgrading_ember
     @text_from_file = File.read(Rails.root.join('The Meta Guide to Upgrading Ember.md'))
   end
