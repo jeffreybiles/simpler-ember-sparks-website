0ca6821b1d1d46fb5b675cea91d03d3835f574d7
jeffreybiles <bilesjeffrey@gmail.com>
2016-03-18 11:33:18 -0500

    Show the 3 selected series

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @posts = Post.all.published.recent_first
+    @tags = [Tag.find(66), Tag.find(76), Tag.find(81)] #later do something with weighted randomness
   end
 end
