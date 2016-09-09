1dbcb5ae9ce49fb99f76814337ecd468d3f92f2d
jeffreybiles <bilesjeffrey@gmail.com>
2016-09-09 00:10:35 -0500

    update series on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @posts = Post.all.published.recent_first
-    @tags = [Tag.find(80), Tag.find(76), Tag.find(68)] #later do something with weighted randomness
+    @tags = [Tag.find(88), Tag.find(86), Tag.find(43)] #later do something with weighted randomness
   end
 end
