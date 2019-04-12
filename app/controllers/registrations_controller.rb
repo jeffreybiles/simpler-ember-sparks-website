e017bd4741e98bc21d28a59edb6af46b175abd3c
jeffreybiles <bilesjeffrey@gmail.com>
2019-04-12 05:11:10 -0500

    new signups no longer go straight to mailchimp

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
   def create
     puts 'were in the create'
     super do |resource|
-      ZapierRuby::Zapper.new(:signup).zap(email: resource.email, subscribed: false)
+      # ZapierRuby::Zapper.new(:signup).zap(email: resource.email, subscribed: false)
     end
   end
 end
